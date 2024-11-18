b79c56cb46a121a1d35287acc63a8ec9b86bc0c0
mohanad <mohanad_u@yahoo.com>
2024-11-18 16:49:15 +0200

    final

---
+++ sanity/schemas/category.ts
@@ -0,0 +1,10 @@
+export default {
+    name:'category',
+    type:'document',
+    title:'categories',
+    fields:[{
+        name:'name',
+        title:'Name of Category',
+        type:'string',
+    }]
+}
