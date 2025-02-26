a6276ef0927b8a381cbafce11d36e924a46d09e9
mohanad <mohanad_u@yahoo.com>
2025-02-26 12:12:57 +0200

    FINAL

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -24,6 +25,7 @@ export default function RootLayout({
           <Navbar />
           <ShoppingCartModal />
           {children}
+          <SpeedInsights />
         </CartProvider>
       </body>
     </html>
